ea3f6f4b3785381e1090e28fb4d56f98f44568f0
huutvpn <143250398+huutvpn@users.noreply.github.com>
2024-09-09 19:55:08 +0700

    Add files via upload

--- backup/restore.sh
+++ backup/restore.sh
@@ -24,19 +24,19 @@ rm -f backup.zip
 sleep 1
 echo Start Restore
 cd /root/backup
-cp -r passwd /etc/
-cp -r group /etc/
-cp -r shadow /etc/
-cp -r gshadow /etc/
-#cp -r chap-secrets /etc/ppp/
-#cp -r passwd1 /etc/ipsec.d/passwd
-#cp -r crot /var/lib/
-#cp -r sstp /home/
-#cp -r xray /etc/
+cp passwd /etc/
+cp group /etc/
+cp shadow /etc/
+cp gshadow /etc/
+cp chap-secrets /etc/ppp/
+cp passwd1 /etc/ipsec.d/passwd
+cp -r crot /var/lib/
+cp -r sstp /home/
+cp -r xray /etc/
 cp -r trojan-go /etc/
 cp -r shadowsocksr /usr/local/
 cp -r public_html /home/vps/
-cp -r crontab /etc/
+cp crontab /etc/
 strt
 rm -rf /root/backup
 rm -f backup.zip
